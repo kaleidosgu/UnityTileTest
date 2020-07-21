c2fd2deee1a54e110332dd47ad5b5e6cd08da6f5
kaleidos <gjcaoyi@163.com>
2020-07-21 21:31:12 +0800

    测试代码添加

--- Assets/Scripts/LoadTimeMap.cs
+++ Assets/Scripts/LoadTimeMap.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+//using YamlTest;
 //using YamlDotNet.Serialization;
 //using YamlDotNet.Samples.Helpers;
+public class subClass
+{
+    public subClass()
+    {
+        babaloka = 5;
+    }
+    public int babaloka { get; set; }
+}
+public class TestClass
+{
+    public int  testInt { get; set; }
+    public string testString { get; set; }
+    //public Vector2 test { get; set; }
+    public subClass ta { get; set; }
+    public Dictionary<string,int> _baba { get; set; }
+    public List<Dictionary<string, string>> ooo { get; set; }
+    //public int[] arrayInt { get; set; }
+}
 
 public class LoadTimeMap : MonoBehaviour
 {
@@ -30,6 +51,26 @@ public class LoadTimeMap : MonoBehaviour
             int nStartIdx = str.IndexOf("Tilemap", 0);
             int nEndIdx = str.IndexOf("---", nStartIdx);
             string strTileMapPaletteData = str.Substring(nStartIdx, nEndIdx - nStartIdx);
+            string strFinal = "---\n" + strTileMapPaletteData;
+
+            strFinal = @"---
+TestTest:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 2193453691686714506}
+  m_Enabled: 1";
+
+            var input = new StringReader(strFinal);
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+
+            var order = deserializer.Deserialize<TestOrder>(input);
+
+
             Debug.Log(str);
         }
         catch (FileNotFoundException e)
@@ -41,7 +82,25 @@ public class LoadTimeMap : MonoBehaviour
     public void TestYaml()
     {
         var serializer = new SerializerBuilder().Build();
-        //var yaml = serializer.Serialize(receipt);
+        TestClass _testIns = new TestClass();
+        _testIns.testInt = 15;
+        _testIns.testString = "1234";
+        _testIns.ta = new subClass();
+        _testIns._baba = new Dictionary<string, int>();
+        _testIns._baba.Add("0", 15);
+        _testIns._baba.Add("1", 25);
+        _testIns.ooo = new List<Dictionary<string, string>>();
+        Dictionary<string, string> test = new Dictionary<string, string>();
+        test.Add("1", "a");
+        test.Add("2", "b");
+        _testIns.ooo.Add(test);
+        test = new Dictionary<string, string>();
+        test.Add("3", "c");
+        test.Add("4", "d");
+        _testIns.ooo.Add(test);
+        //_testIns.test = new Vector2(1, 2);
+        var yaml = serializer.Serialize(_testIns);
+        int a = 0;
         //output.WriteLine(yaml);
     }
 }
---
+++ Assets/Scripts/YamlClassDefine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace YamlTest
+//{
+    public class TestTest
+{
+        public int m_ObjectHideFlags { get; set; }
+    }
+//}
+
+    public class TestOrder
+{
+    public TestTest TestTest { get; set; }
+}
