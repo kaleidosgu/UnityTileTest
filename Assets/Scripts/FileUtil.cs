47a1133f70a5af23e46715c8e1f0fb7b9ef91602
kaleidos <gjcaoyi@163.com>
2020-07-21 23:34:56 +0800

    之前的代码添加

---
+++ Assets/Scripts/FileUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class FileUtil 
+{
+    public static string GetFileContextByPath(string strPath)
+    {
+        StreamReader reader = new StreamReader(strPath);
+        string str = reader.ReadToEnd();
+        reader.Close();
+        return str;
+    }
+}
---
+++ Assets/Scripts/LoadUnityYaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadUnityYaml : MonoBehaviour
+{
+    public string YamlFileName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string strContent = FileUtil.GetFileContextByPath(YamlFileName);
+        int a = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
