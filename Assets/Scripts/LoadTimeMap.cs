8b850d02125983c189fc9240d167f0f83ea2b188
kaleidos <gjcaoyi@163.com>
2020-07-21 09:41:54 +0800

    之前的读取文件没有上传，这里添加一个新的。

---
+++ Assets/Scripts/LoadTimeMap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadTimeMap : MonoBehaviour
+{
+    public string TileMapPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("LoadFileTest")]
+    public void LoadFile()
+    {
+        try
+        {
+            string str = UtilFile.ReadStringFromFile(TileMapPath);
+            Debug.Log(str);
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/Util/UtilFile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class UtilFile 
+{
+    public static string ReadStringFromFile(string strPath)
+    {
+        StreamReader reader = new StreamReader(strPath);
+        string strRet = reader.ReadToEnd();
+        reader.Close();
+        return strRet;
+    }
+}
