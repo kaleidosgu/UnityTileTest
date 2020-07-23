333535e09e1576d6136f22f94d9ea26aa148e23a
kaleidos <gjcaoyi@163.com>
2020-07-23 09:26:39 +0800

    把整个Tile的prefab中的tilemap数据反序列化

--- Assets/Scripts/YamlClassDefine.cs
+++ Assets/Scripts/YamlClassDefine.cs
@@ -23,6 +23,67 @@ public class TilemapData
     public AnimatedTile m_AnimatedTiles { get; set; }
     public List<TileAsset> m_TileAssetArray { get; set; }
     public List<TileAsset> m_TileSpriteArray { get; set; }
+
+    public List<TileMatrix> m_TileMatrixArray { get; set; }
+
+    public List<TileColorElement> m_TileColorArray { get; set; }
+    public int[] m_TileObjectToInstantiateArray { get; set; }
+    public int m_AnimationFrameRate { get; set; }
+    public ColorData m_Color;
+    public PositionData m_Origin { get; set; }
+    public SizeData m_Size { get; set; }
+    public PositionData m_TileAnchor { get; set; }
+    public int m_TileOrientation { get; set; }
+    public TileMatrixData m_TileOrientationMatrix { get; set; }
+}
+public class SizeData
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public int z { get; set; }
+}
+
+public class PositionData
+{
+    public float x { get; set; }
+    public float y { get; set; }
+    public float z { get; set; }
+}
+public class ColorData
+{
+    public float r { get; set; }
+    public float g { get; set; }
+    public float b { get; set; }
+    public float a { get; set; }
+}
+public class TileColorElement
+{
+    public int m_RefCount { get; set; }
+    public ColorData m_Data { get; set; }
+}
+public class TileMatrix
+{
+    public int m_RefCount { get; set; }
+    public TileMatrixData m_Data { get; set; }
+}
+public class TileMatrixData
+{
+    public int e00 { get; set; }
+    public int e01 { get; set; }
+    public int e02 { get; set; }
+    public int e03 { get; set; }
+    public int e10 { get; set; }
+    public int e11 { get; set; }
+    public int e12 { get; set; }
+    public int e13 { get; set; }
+    public int e20 { get; set; }
+    public int e21 { get; set; }
+    public int e22 { get; set; }
+    public int e23 { get; set; }
+    public int e30 { get; set; }
+    public int e31 { get; set; }
+    public int e32 { get; set; }
+    public int e33 { get; set; }
 }
 public class AnimatedTile
 {
